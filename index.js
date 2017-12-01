ec9095df014086bdd9affa859f9453a6c46939df
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2017-12-01 19:41:57 +0900

    update devDependencies
    Highlight: spaces -> tabs https://github.com/shinnn/eslint-config/commit/e92384d999f10fd0e477e6ada6b979c48977a2f7

--- index.js
+++ index.js
@@ -5,22 +5,22 @@ const npmCliDir = require('npm-cli-dir');
 const resolveFrom = require('resolve-from');
 
 module.exports = function resolveFromNpm(moduleId) {
-  return npmCliDir().then(fromDir => {
-    if (typeof moduleId !== 'string') {
-      return Promise.reject(new TypeError(`Expected a module ID to resolve from npm directory (${fromDir}), but got ${
-        inspectWithKind(moduleId)
-      }.`));
-    }
+	return npmCliDir().then(fromDir => {
+		if (typeof moduleId !== 'string') {
+			return Promise.reject(new TypeError(`Expected a module ID to resolve from npm directory (${fromDir}), but got ${
+				inspectWithKind(moduleId)
+			}.`));
+		}
 
-    const result = resolveFrom.silent(fromDir, moduleId);
+		const result = resolveFrom.silent(fromDir, moduleId);
 
-    if (result === null) {
-      const err = new Error(`Cannot find module \`${moduleId}\` from npm directory (${fromDir}).`);
-      err.code = 'MODULE_NOT_FOUND';
+		if (result === null) {
+			const err = new Error(`Cannot find module \`${moduleId}\` from npm directory (${fromDir}).`);
+			err.code = 'MODULE_NOT_FOUND';
 
-      return Promise.reject(err);
-    }
+			return Promise.reject(err);
+		}
 
-    return result;
-  });
+		return result;
+	});
 };
--- test.js
+++ test.js
@@ -10,76 +10,76 @@ const rmfr = require('rmfr');
 const test = require('tape');
 
 test('resolveFromNpm()', async t => {
-  t.plan(8);
+	t.plan(8);
 
-  const symlinkPath = join(__dirname, 'tmp.js');
+	const symlinkPath = join(__dirname, 'tmp.js');
 
-  await rmfr(symlinkPath);
-  await promisify(symlink)(__filename, symlinkPath);
+	await rmfr(symlinkPath);
+	await promisify(symlink)(__filename, symlinkPath);
 
-  const dir = await npmCliDir();
+	const dir = await npmCliDir();
 
-  resolveFromNpm('semver').then(semverPath => {
-    const semver = require(semverPath);
+	resolveFromNpm('semver').then(semverPath => {
+		const semver = require(semverPath);
 
-    t.equal(
-      semver.compare('2.0.0-rc1', '1.99.2-beta'),
-      1,
-      'should resolve the path of a module entry point.'
-    );
-  }).catch(t.fail);
+		t.equal(
+			semver.compare('2.0.0-rc1', '1.99.2-beta'),
+			1,
+			'should resolve the path of a module entry point.'
+		);
+	}).catch(t.fail);
 
-  resolveFromNpm('request/package.json').then(requestPackageJsonPath => {
-    t.equal(
-      require(requestPackageJsonPath).name,
-      'request',
-      'should resolve the path inside a module.'
-    );
-  }).catch(t.fail);
+	resolveFromNpm('request/package.json').then(requestPackageJsonPath => {
+		t.equal(
+			require(requestPackageJsonPath).name,
+			'request',
+			'should resolve the path inside a module.'
+		);
+	}).catch(t.fail);
 
-  resolveFromNpm('./package.json').then(npmPackageJsonPath => {
-    t.equal(
-      require(npmPackageJsonPath).main,
-      './lib/npm.js',
-      'should resolve the path from a relative path.'
-    );
-  }).catch(t.fail);
+	resolveFromNpm('./package.json').then(npmPackageJsonPath => {
+		t.equal(
+			require(npmPackageJsonPath).main,
+			'./lib/npm.js',
+			'should resolve the path from a relative path.'
+		);
+	}).catch(t.fail);
 
-  resolveFromNpm(symlinkPath).then(resolvedSymlinkPath => {
-    t.equal(
-      resolvedSymlinkPath,
-      __filename,
-      'should resolve symbolic links.'
-    );
-  }).catch(t.fail);
+	resolveFromNpm(symlinkPath).then(resolvedSymlinkPath => {
+		t.equal(
+			resolvedSymlinkPath,
+			__filename,
+			'should resolve symbolic links.'
+		);
+	}).catch(t.fail);
 
-  resolveFromNpm('package.json').then(t.fail, ({code, message}) => {
-    t.equal(
-      message,
-      `Cannot find module \`package.json\` from npm directory (${dir}).`,
-      'should fail when it cannot resolve a path.'
-    );
+	resolveFromNpm('package.json').then(t.fail, ({code, message}) => {
+		t.equal(
+			message,
+			`Cannot find module \`package.json\` from npm directory (${dir}).`,
+			'should fail when it cannot resolve a path.'
+		);
 
-    t.equal(
-      code,
-      'MODULE_NOT_FOUND',
-      'should add `code` property to the error when it cannot resolve a path.'
-    );
-  }).catch(t.fail);
+		t.equal(
+			code,
+			'MODULE_NOT_FOUND',
+			'should add `code` property to the error when it cannot resolve a path.'
+		);
+	}).catch(t.fail);
 
-  resolveFromNpm(1).then(t.fail, err => {
-    t.equal(
-      err.name,
-      'TypeError',
-      'should be rejected with a type error when it takes a non-string argument.'
-    );
-  }).catch(t.fail);
+	resolveFromNpm(1).then(t.fail, err => {
+		t.equal(
+			err.name,
+			'TypeError',
+			'should be rejected with a type error when it takes a non-string argument.'
+		);
+	}).catch(t.fail);
 
-  resolveFromNpm().then(t.fail, ({message}) => {
-    t.equal(
-      message,
-      `Expected a module ID to resolve from npm directory (${dir}), but got undefined.`,
-      'should be rejected with a type error when it takes no arguments.'
-    );
-  }).catch(t.fail);
+	resolveFromNpm().then(t.fail, ({message}) => {
+		t.equal(
+			message,
+			`Expected a module ID to resolve from npm directory (${dir}), but got undefined.`,
+			'should be rejected with a type error when it takes no arguments.'
+		);
+	}).catch(t.fail);
 });
